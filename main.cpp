b25f965e848bf062bb1970189e28d0d7ab0d0ba7
TanyaAkulich <akulich.tanya@gmail.com>
2019-10-26 08:26:27 +0300

    Fix function result writting

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <condition_variable>
 #include <thread>
 #include <fstream>
+#include <string>
 
 std::condition_variable cv;
 std::mutex cv_m;
@@ -10,35 +11,39 @@ bool done = false;
 int current_function_value = 1;
 std::fstream function_result;
 
-void init_result_file();
-
-void write_function_result();
-
 const char filename[] = "../results";
 
-
-void result_listener() {
-    init_result_file();
-    std::unique_lock<std::mutex> lock(cv_m);
-
-    while (!done) {
-        while (notified) {
-            notified = false;
-            write_function_result();
-        }
-        cv.wait(lock);
-    }
+void init_file(char filename[]) {
+    function_result.open(filename,  std::fstream::in | std::fstream::out | std::fstream::trunc);
+    function_result.close();
 }
 
-void write_function_result() {
+void log_writer() {
+    const char filename[] = "../log";
+
+    init_file(const_cast<char *>(filename));
+}
+
+void write_function_result(char filename[]) {
     function_result.open(filename, std::fstream::in | std::fstream::out | std::fstream::app);
     function_result << current_function_value << "\n";
     function_result.close();
 }
 
-void init_result_file() {
-    function_result.open(filename,  std::fstream::in | std::fstream::out | std::fstream::trunc);
-    function_result.close();
+void result_listener() {
+    const char filename[] = "../results";
+
+    init_file(const_cast<char *>(filename));
+
+    std::unique_lock<std::mutex> lock(cv_m);
+
+    while (!done) {
+        while (notified) {
+            notified = false;
+            write_function_result(const_cast<char *>(filename));
+        }
+        cv.wait(lock);
+    }
 }
 
 int factorial_calculation(int n) {
@@ -49,19 +54,21 @@ int factorial_calculation(int n) {
 
         current_function_value *= j;
         notified = true;
-        cv.notify_one();
+        cv.notify_all();
     }
 
     done = true;
-    cv.notify_one();
+    cv.notify_all();
 }
 
 int main() {
     std::thread factorial(factorial_calculation, rand() % 10);
     std::thread result(result_listener);
+    std::thread log(log_writer);
 
     factorial.join();
     result.join();
+    log.join();
 
     std::cout << "complete";
     return 0;
